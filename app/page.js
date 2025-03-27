0ce1020ed846c2f9bfb0cf3f78c6a554dafc7302
Mikethestrike23 <michael.lalonde1@edu.sait.ca>
2025-03-27 17:29:53 -0600

    First commit

--- app/page.js
+++ app/page.js
@@ -3,32 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>CPRG306: Web Development 2 - Assignments</h1>
-        <Link href="/week-2">Week 2 Assignment</Link>
-      <h2>
-        <Link href="/week-3">Week 3 Assignment</Link>
-      </h2>
-      
-      <h2>
-      <Link href="week-4">Week 4 Assignment</Link>
-     </h2>
-     <h2>
-      <Link href="week-5">Week 5 Assignment</Link>
-     </h2>
-     <h2>
-      <Link href="week-6">Week 6 Assignment</Link>
-     </h2>
-     <h2>
-      <Link href="week-7">Week 7 Assignment</Link>
-     </h2>
-     <h2>
-      <Link href="week-8">Week 8 Assignment</Link>
-     </h2>
-     <h2>
-      <Link href="week-9">Week 9 Assignment</Link>
-     </h2>
-    </div>
-  );
+<h1>Level Up</h1>
+
+
 }
--- app/week-2/student-info.js
+++
@@ -1,8 +0,0 @@
-export default function StudentInfo({ name, github }) {
-    return (
-        <div>
-           <h1>{name}</h1>
-            <a href={github}>{github}</a>
-        </div>
-    );
-}
