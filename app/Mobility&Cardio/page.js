c41f5ebc3f9d7b77dc41d90305a42f774a86421c
Zumar Syed Bukhari <164260201+Zumar15@users.noreply.github.com>
2025-04-01 20:54:07 -0600

    Adding Mobility and Cardio Page

--- app/Mobility&Cardio/page.js
+++ app/Mobility&Cardio/page.js
@@ -1,9 +1,28 @@
-
+import Link from "next/link";
 
 export default function MobilityCardio() {
   return (
     <div>
-      <h1>Mobility & Cardio</h1>
+      <br></br>
+      <Link
+          href="/Stretching"
+          className="p-4 rounded-lg m-4 bg-orange-300 w-48"
+        >
+          Stretching Exercises
+      </Link>
+      <Link
+          href="/Abs"
+          className="p-4 rounded-lg m-4 bg-red-300 w-48"
+        >
+          Abs Exercises
+      </Link>
+      <Link
+          href="/Cardio"
+          className="p-4 rounded-lg m-4 bg-yellow-300 w-48"
+        >
+          Cardio Exercises
+      </Link>
     </div>
   );
 }
+//https://www.w3schools.com/TAGS/tag_br.asp
